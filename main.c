700ea4af6ee2387436f68aa1ed64bdd46e14e9f1
CooolRyan <nawonho99@gmail.com>
2022-05-09 22:30:48 -0700

    file size and block size comparation

--- main.c
+++ main.c
@@ -49,12 +49,38 @@ void filetime2(){
 
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(){
-    
+	const char* t1="./text1";
+	const char* t2="./text2";
+	stat(t1,stat1);
+	stat(t2,stat2);
+	size1=stat1->st_size;
+	size2=stat2->st_size;
+
+	print("size compare\n");
+	if(size1>size2)
+		print("text1 is bigger\n");
+	else if (size1<size2)
+		print("text2 is bigger\n");
+	else
+		print("size are equal\n");
 }
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
 void blockcmp(){
-    
+   	const char* t1="./text1";
+	const char* t2="./text2";
+	stat(t1,stat1);
+	stat(t2,stat2);
+	block1=stat1->st_blksize;
+	block2=stat2->st_blksize;
+
+	print("block compare\n");
+	if(block1>block2)
+		print("text1 is bigger\n");
+	else if (block1<block2)
+		print("text2 is bigger\n");
+	else
+		print("size are equal\n");
 }
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
