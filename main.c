1743b1da3f4d72e21378000a560e3b3e1557ee62
doomdabo <bbo1209@naver.com>
2022-05-11 23:28:27 -0700

    datecmp, timecmp add

--- main.c
+++ main.c
@@ -29,22 +29,22 @@ int main(){
 
 //파일 1의 정보를 가져오는 함수 작성
 void filestat1(){
-    
+
 }
 
 //파일 2의 정보를 가져오는 함수 작성
 void filestat2(){
-    
+
 }
 
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(){
-    
+
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(){
-    
+
 }
 
 //두 개의 파일 크기를 비교하는 함수 작성
@@ -59,10 +59,25 @@ void blockcmp(){
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
 void datecmp(){
-    
+	printf("date compare\n");
+	if(time1->tm_mon>time2->tm_mon) printf("text1 is early\n");
+	else if(time1->tm_mon<time2->tm_mon) printf("text2 is early\n");
+	else if(time1->tm_mon==time2->tm_mon) {
+		if(time1->tm_mday<time2->tm_mday) printf("text1 is early\n");
+		else if(time1->tm_mday>time2->tm_mday) printf("text2 is early\n");
+		else if(time1->tm_mday==time2->tm_mday) printf("same date\n");
+	}
 }
 
 //두 개의 파일 수정 시간을 비교하는 함수 작성
 void timecmp(){
-    
+	printf("time compare\n");
+	if(time1->tm_hour>time2->tm_hour) printf("text1 is early\n");
+	else if(time1->tm_hour<time2->tm_hour) printf("text2 is early\n");
+	else if(time1->tm_hour==time2->tm_hour) {
+		if(time1->tm_min<time2->tm_min) printf("text1 is early\n");
+		else if(time1->tm_min>time2->tm_min) printf("text2 is early\n");
+		else if(time1->tm_min==time2->tm_min) printf("same date\n");
+	}
 }
+
